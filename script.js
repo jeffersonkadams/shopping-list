8aa15ff3871b4a57005e61e141b7d92b6c2051ce
Jeff <jeffersonkadams@gmail.com>
2018-05-11 18:50:47 -0400

    started the shopping list. all vanilla js, no angular

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("loaded!")
+var stores = [];
+
+
+function addNewStore(){
+  var newStore = document.getElementById("newStore").value;
+  stores.push(newStore);
+  console.log(stores);
+}
+function addNewItem(){
+  var newItem = document.getElementById("newItem").value;
+}
